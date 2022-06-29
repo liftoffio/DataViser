ac820845478c58c6c820dcc04378a99f2374292b
Frank Liu <frank.liu@vungle.com>
2022-06-29 13:37:05 +0800

    feat: add scale control

--- dataviser-grafana-panel-plugin/src/MyScene.tsx
+++ dataviser-grafana-panel-plugin/src/MyScene.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { MapboxScene } from "@antv/l7-react";
-import MyPointLayer from "./layers/MyPointLayer";
+import React from 'react';
+import { MapboxScene, Control } from '@antv/l7-react';
+import MyPointLayer from './layers/MyPointLayer';
 
-const MyScene = React.memo(function Map(props: {data: Array<any>}) {
+const MyScene = React.memo(function Map(props: { data: any[] }) {
   const { data } = props;
   return (
     <>
@@ -10,18 +10,19 @@ const MyScene = React.memo(function Map(props: {data: Array<any>}) {
         map={{
           center: [104.288144, 31.239692],
           pitch: 45.210526315789465,
-          style: "dark",
-          zoom: 2.4
+          style: 'dark',
+          zoom: 2.4,
         }}
         style={{
-          position: "absolute",
+          position: 'absolute',
           top: 0,
           left: 0,
           right: 0,
-          bottom: 0
+          bottom: 0,
         }}
       >
         <MyPointLayer data={data} />
+        <Control type="scale" position="bottomright" />
       </MapboxScene>
     </>
   );
